3092889d9681b7ca1b9d602e2e1fda95548c9f58
Kimberly Eddinger <kimberly.c.eddinger@gmail.com>
2020-07-20 11:41:48 -0700

    Fixes bug where index was a very large negative number.

--- db/postgres/model.js
+++ db/postgres/model.js
@@ -1,27 +1,28 @@
 const db = require('./index.js');
 
 const getRandomId = (firstId, lastId, amount, direction) => {
+  let amountToInt = parseInt(amount)
   if (direction === 'ascend') {
-    let max = lastId - amount;
+    let max = lastId - amountToInt;
     return Math.floor(Math.random() * (max - firstId)) + firstId;
   } else if (direction === 'descend') {
-    let min = firstId + amount;
+    let min = firstId + amountToInt;
     return Math.floor(Math.random() * (lastId - min)) + min;
   } else {
-    return 'error';
+    console.log('error in getRandomID');
   }
 }
 
 const dbHelpers = {
   getAll: (callback) => {
     let startIndex = getRandomId(9000000, 10000000, 4, 'descend');
-    db.query(`select * from mainsuggest where id >= ${startIndex} order by id limit 4;`, (err, res) => {
+    db.query(`select * from mainsuggest where id <= ${startIndex} order by id desc limit 4;`, (err, res) => {
       err ? callback(err, null) : callback(null, res);
     })
   },
   getShades: (pullAmount, callback) => {
     let startIndex = getRandomId(1800000, 2000000, pullAmount, 'descend');
-    db.query(`select * from shades where id >= ${startIndex} order by id limit ${pullAmount};`, (err, res) => {
+    db.query(`select * from shades where id <= ${startIndex} order by id desc limit ${pullAmount};`, (err, res) => {
       err ? callback(err, null) : callback(null, res);
     })
   },
