1d98cfcdc73d087115a2f4f323d234f92225794b
Kimberly Eddinger <kimberly.c.eddinger@gmail.com>
2020-07-20 10:58:19 -0700

    Fixed queries that need to descend.

--- db/postgres/model.js
+++ db/postgres/model.js
@@ -20,13 +20,13 @@ const dbHelpers = {
     })
   },
   getShades: (pullAmount, callback) => {
-    let startIndex = getRandomId(1800000, 2000000, pullAmount, 'ascend');
+    let startIndex = getRandomId(1800000, 2000000, pullAmount, 'descend');
     db.query(`select * from shades where id >= ${startIndex} order by id limit ${pullAmount};`, (err, res) => {
       err ? callback(err, null) : callback(null, res);
     })
   },
   getQuickview: (callback) => {
-    let startIndex = getRandomId(3600000, 4000000, 5, 'descend');
+    let startIndex = getRandomId(3600000, 4000000, 5, 'ascend');
     db.query(`select * from quickview where id >= ${startIndex} order by id limit 5;`, (err, res) => {
       err ? callback(err, null) : callback(null, res);
 
