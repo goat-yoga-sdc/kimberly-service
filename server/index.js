21bd025fe063445a69574ce50d0a0ba29616b112
Kimberly Eddinger <kimberly.c.eddinger@gmail.com>
2020-07-21 16:41:31 -0700

    Fixes front end bug, adds new relic and k6.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
+const rewrelic = require('newrelic')
 const express = require ('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const router = require('./router.js');
-
 
 const port = 3050;
 const app = express();
@@ -13,11 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(morgan('dev'));
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+
+
 app.use('/', express.static(path.join(__dirname, '../client/dist')))
 
 app.use('/products', router);
 
-app.get('/products/suggested', (req, res) => {
-    res.send('Successful')
-})
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+
